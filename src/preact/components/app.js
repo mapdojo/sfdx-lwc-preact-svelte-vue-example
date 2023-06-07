63a5c3e45c6980ebac9f6f398633c6f3b2d0326f
Brad Browne <brad@mapdojo.com>
2023-06-08 09:42:31 +1000

    Added Leaflet

--- src/preact/components/app.js
+++ src/preact/components/app.js
@@ -1,10 +1,16 @@
 // `h` doesn't seemed to be used, but it is key for the Babel transform.
 import { h, Component } from "preact";
+import * as leaflet from "leaflet";
 
 import List from "./list";
 
 export default class App extends Component {
     render(props) {
+        // initialize the map on the "map" div with a given center and zoom
+        var map = L.map("map", {
+            center: [51.505, -0.09],
+            zoom: 13
+        });
         return (
             <div id="app">
                 Data from outer property: {props.title}
