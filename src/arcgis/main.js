bdbba73df0e4d4d2baafc0c59a5f90eabb08d60d
Brad Browne <brad@mapdojo.com>
2023-06-15 16:57:30 +1000

    Added Popup, Zoom and Attribution widgets

--- src/arcgis/main.js
+++ src/arcgis/main.js
@@ -1,5 +1,8 @@
 import WebMap from "@arcgis/core/WebMap";
 import MapView from "@arcgis/core/views/MapView";
 import Search from "@arcgis/core/widgets/Search";
+import Popup from "@arcgis/core/widgets/Popup";
+import Zoom from "@arcgis/core/widgets/Zoom";
+import Attribution from "@arcgis/core/widgets/Attribution";
 
-export { WebMap, MapView, Search };
+export { WebMap, MapView, Search, Popup, Zoom, Attribution };
