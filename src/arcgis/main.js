17c5b0ec430caabd4386c083ac82ae7b5739e31c
Brad Browne <brad@mapdojo.com>
2023-06-19 11:16:51 +1000

    Fixed esriConfig.assetsPath

--- src/arcgis/main.js
+++ src/arcgis/main.js
@@ -6,6 +6,7 @@ import Zoom from "@arcgis/core/widgets/Zoom";
 import Attribution from "@arcgis/core/widgets/Attribution";
 import esriConfig from "@arcgis/core/config";
 
-// esriConfig.assetsPath = "./core/assets";
+esriConfig.assetsPath =
+    "/assets/project/58f8aa9363/staticresources/arcgis/core/assets";
 
 export { WebMap, MapView, Search, Popup, Zoom, Attribution, esriConfig };
